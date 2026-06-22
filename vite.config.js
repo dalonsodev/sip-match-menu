644665b78f226bf5c71d3eaf56963d2d6d8a3fbb
dalonso <masdavidalonso@gmail.com>
2026-06-22 17:47:18 +0200

    chore: revert to img-based SVG, remove vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import vitePluginSvgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-    plugins: [
-      react(),
-      vitePluginSvgr()
-    ],
+    plugins: [react()],
     server: {
       host: '0.0.0.0', // ← This allows for external connections
       port: 5173,
