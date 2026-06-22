70f2a80483b751022a2064b95440c88dbf6d34a3
dalonso <masdavidalonso@gmail.com>
2026-06-22 17:20:14 +0200

    chore: install and configure vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginSvgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-    plugins: [react()],
+    plugins: [
+      react(),
+      vitePluginSvgr()
+    ],
     server: {
       host: '0.0.0.0', // ← This allows for external connections
       port: 5173,
